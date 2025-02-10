1af0ac7c0a3a32296a3ffd8add4db0bbe47bb744
Cinition <cornee.n@gmail.com>
2025-02-10 22:58:37 +0100

    Setup/Base_code

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,54 @@
 const std = @import("std");
+const vk = @import("vulkan");
+const wio = @import("wio");
+
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+pub const allocator = gpa.allocator();
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
+
+var window: wio.Window = undefined;
+
+const HelloTriangleApplication = struct {
+    pub fn run(self: *HelloTriangleApplication) !void {
+        _ = self;
+        try initWindow();
+        initVulkan();
+        try wio.run(mainLoop);
+        cleanup();
+    }
+
+    fn initWindow() !void {
+        try wio.init(allocator, .{});
+        window = try wio.createWindow(.{
+            .title = "Vulkan",
+            .size = .{ .width = WIDTH, .height = HEIGHT },
+        });
+    }
+
+    fn initVulkan() void {}
+
+    fn mainLoop() !bool {
+        while (window.getEvent()) |event| {
+            switch (event) {
+                .close => {
+                    return false;
+                },
+                else => {},
+            }
+        }
+
+        return true;
+    }
+
+    fn cleanup() void {
+        window.destroy();
+        wio.deinit();
+    }
+};
 
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var app: HelloTriangleApplication = .{};
+    try app.run();
 }
